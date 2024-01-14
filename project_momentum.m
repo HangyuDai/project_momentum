49bb0dec1851c38019c7e52c717608ec08a281fd
HangyuDai <daihy1119@gmail.com>
2024-01-14 18:28:34 +0800

    Update project_momentum.m

--- project_momentum.m
+++ project_momentum.m
@@ -77,7 +77,7 @@ end
 return_full.mom_label=rowfun(@mom_bucket_5,return_full(:,{'pr_return','mom20','mom40','mom60'...
                 ,'mom80'}),'OutputFormat','cell');
    
-% ceate equal-weighted portfolio
+% create equal-weighted portfolio
 
 return_full.ew=ones(size(return_full,1),1);        
            
@@ -102,7 +102,8 @@ E=nanmean(table2array(mom_factors(:,6)))*100;
 
 fprintf('The average return for the low previous return group is %4.3f percent per month \n',A)
 
-fprintf('The average return for the high previous return group is %4.3f percent per month \n',E)
+fprintf(['The average return for the high previ' ...
+    'ous return group is %4.3f percent per month \n'],E)
 
 
 % The output shows that: 
@@ -112,3 +113,37 @@ fprintf('The average return for the high previous return group is %4.3f percent 
 % based on the observed return spread, it seems that there is evidence of momentum in Chinese stock markets. 
 % The positive return spread suggests a potential momentum effect
 % Past winners continue to outperform past losers over the specified holding periods.
+
+%% PCA and MOM factor
+
+% Let us illusrate this for $K=3$
+
+mom_pca=table2array(mom_factors(:,2:6));
+
+[coefMatrix score latnt tsquared explainedVar]=pca(mom_pca);
+
+factors=mom_pca*coefMatrix(:,1:5);
+
+plot(coefMatrix(:,1:3),'-x');
+legend('First','Second','Third')
+
+
+mom=mom_pca(:,5)-mom_pca(:,1);
+
+
+corr(factors(:,1),mom);
+
+corr(factors(:,2),mom);
+
+corr(factors(:,3),mom);
+
+corr(factors(:,4),mom);
+
+corr(factors(:,5),mom);
+
+
+% The second factor is strongly related to MOM, suggesting that there is
+% slope structure for returns sorted by previous stock returns.
+
+% This first factor just controls the level and is not useful to explain
+% why high previous stock returns earn low returns in the cross-section
